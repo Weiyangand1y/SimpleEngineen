72169e81c3953926b8fe5efdb09a3ba01a88c1c7
wy <21wychen@stu.edu.cn>
2024-04-24 09:41:42 +0800

    [feat]完善TcpClient，开始创建TcpServer，可以互相发消息

--- L2/Lib/Net/TcpClient.h
+++ L2/Lib/Net/TcpClient.h
@@ -6,16 +6,53 @@ using asio::ip::tcp;
 
 class GameClient {
 public:
-    GameClient() : io_context_(), socket_(io_context_), resolver_(io_context_) {}
+    GameClient() : io_context_(), socket_(io_context_), resolver_(io_context_), connected_(false) {}
 
     void init(const std::string& server_address, const std::string& port) {
-        asio::connect(socket_, resolver_.resolve(server_address, port));
+        server_address_ = server_address;
+        port_ = port;
     }
 
     virtual void handleRead(const std::string& message) = 0;
     virtual void handleError(const std::string& error_message) = 0;
 
-    void startAsyncRead() {
+    void connect_to_server() {
+        resolver_.async_resolve(server_address_, port_,
+            [this](std::error_code ec, tcp::resolver::results_type endpoints) {
+                if (!ec) {
+                    asio::async_connect(socket_, endpoints,
+                        [this](std::error_code ec, tcp::endpoint) {
+                            if (!ec) {
+                                connected_ = true;
+                                start_async_read();
+                            } else {
+                                handleError(ec.message());
+                            }
+                        });
+                } else {
+                    handleError(ec.message());
+                }
+            });
+    }
+
+    void disconnect() {
+        if (connected_) {
+            socket_.close();
+            connected_ = false;
+        }
+    }
+
+    void reconnect(const std::string& server_address, const std::string& port) {
+        disconnect();
+        init(server_address, port);
+        connect_to_server();
+    }
+
+    void start_async_read() {
+        if (!connected_) {
+            handleError("Not connected to server.");
+            return;
+        }
         asio::async_read_until(socket_, receive_buffer_, "\n",
             [this](std::error_code ec, std::size_t length) {
                 if (!ec) {
@@ -25,18 +62,24 @@ public:
                     handleRead(received_message);
 
                     // Continue reading
-                    startAsyncRead();
+                    start_async_read();
                 } else {
                     handleError(ec.message());
+                    disconnect();
                 }
             });
     }
 
     void sendMessage(const std::string& message) {
+        if (!connected_) {
+            handleError("Not connected to server.");
+            return;
+        }
         asio::async_write(socket_, asio::buffer(message.c_str(), message.size() + 1), // Include '\0' in buffer
             [this](std::error_code ec, std::size_t /*length*/) {
                 if (ec) {
                     handleError(ec.message());
+                    disconnect();
                 }
             });
     }
@@ -50,6 +93,9 @@ protected:
     tcp::socket socket_;
     tcp::resolver resolver_;
     asio::streambuf receive_buffer_;
+    std::string server_address_;
+    std::string port_;
+    bool connected_;
 };
 
 class MyGameClient : public GameClient {
@@ -67,4 +113,3 @@ public:
         // Add your custom error handling code here
     }
 };
-
--- Test/test01.cpp
+++ Test/test01.cpp
@@ -4,7 +4,7 @@
 #include "L2/Lib/imgui/MyImGui.h"
 #include "L2/Object/TaskQueue.h"
 #include "L2/Lib/Net/TcpClient.h"
-
+#include "L2/Lib/Net/TcpServer.h"
 
 #include "L1/Object/ScriptObject.h"
 #include "L1/Object/SignalObject.h"
@@ -20,9 +20,19 @@
 
 
 class TestApplication:public Application{
+public:
+    float progress_bar=0.f;
+private:
     char msg[128]={0};
     char to_send[128]={0};
+    char host[32]="127.0.0.1";
+    char port[8]="8888";
+
+    char server_rec_msg[128]={0};
+    char server_msg[128]={0};
+
     MyGameClient client;
+    TcpServer server;
     ImgageLoad image_load;
     ImageCut image_cut;
     ImagePlatter image_platter;
@@ -30,11 +40,12 @@ class TestApplication:public Application{
     ImageDB image_db;
     ImConsole console;
     TimeCutQueue tq;
-    static void test(Application* app){
+    static void test(TestApplication* app){
         std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
         for (int i = 0; i < 100; i++)
         {
             app->get_renderer()->get_texture_db().load("bg"+std::to_string(i),"C:/Users/21wyc/Pictures/pixel/117601008_p0.png");
+            app->progress_bar=(float)i/100;
         }
         std::cout << "test:" << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now()-start).count() << std::endl;
     }
@@ -60,11 +71,16 @@ public:
         client.read_callback=[this](const std::string& message){
             memcpy(msg,message.c_str(),message.size()+1);
         };
+        server.read_callback=[this](const std::string& message){
+            memcpy(server_rec_msg,message.c_str(),message.size()+1);
+        };
+        server.init(8888);
         client.init("127.0.0.1","8888");
-        client.startAsyncRead();
+        client.connect_to_server();
+        client.start_async_read();
     }
     void _run() override{
-
+        server.poll();
         client.poll();
 
         renderer.start_framebuffer("f1");  //--------------------
@@ -77,13 +93,30 @@ public:
 
         MyImGui::static_begin();
         ImGui::Begin("TCP Net");
+        ImGui::BeginChild("Client",ImVec2{500,200},ImGuiChildFlags_Border);
+        ImGui::SeparatorText("Client");
         ImGui::Text("read message:\n%s",msg);
+        ImGui::InputTextWithHint("host","host",host,sizeof(host));
+        ImGui::InputTextWithHint("port","port",port,sizeof(port));
+        if(ImGui::Button("connect")){
+            client.reconnect(host,port);
+        }
         if(ImGui::IsWindowFocused() && ImGui::IsKeyDown(ImGuiKey_Enter)){
             ImGui::SetKeyboardFocusHere();
         }
         if(ImGui::InputText("send",to_send,128,ImGuiInputTextFlags_EnterReturnsTrue)){
-            client.sendMessage(std::string(to_send));
+            client.sendMessage(std::string(to_send)+"\n");
         }
+        ImGui::EndChild();
+        ImGui::BeginChild("Server",ImVec2{500,200},ImGuiChildFlags_Border);
+        ImGui::SeparatorText("Server");
+        ImGui::Text(server_rec_msg);
+        ImGui::InputText("s_msg",server_msg,sizeof(server_msg));
+        if(ImGui::Button("send")){
+            server.sendMessage(std::string(server_msg)+"\n");
+        }
+        ImGui::EndChild();
+        ImGui::SliderFloat("progress",&progress_bar,0,1);
         ImGui::End();
         image_load.render();
         image_cut.render();
